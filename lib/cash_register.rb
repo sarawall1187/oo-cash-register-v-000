a399b6935c85b7bab5411c5e6615b51c5f1b325f
Sara Walker <sarawall1187@gmail.com>
2019-01-15 20:47:03 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -45,47 +45,83 @@
 #   end
    
 # end 
+# class CashRegister
+
+#   attr_accessor :total, :discount, :last_transaction_amount, :items
+
+#   def initialize(discount = 0)
+#     @total = 0
+#     @discount = discount
+#     @items = []
+#   end
+
+#   def add_item(title,price,quantity = 1)
+#     if quantity>1
+#       i=0
+#       while i<quantity
+#         @items << title   
+#         i+=1
+#       end
+#     else
+#       @items << title
+#     end
+#     @total += price*quantity
+#     @last_transaction_amount = @total
+#     @total
+#   end
+
+#   def apply_discount()
+#     if @discount > 0
+#       @discount = @discount/100.to_f
+#       @total = @total - (@total * (@discount))
+#       "After the discount, the total comes to $#{@total.to_i}."
+#     else
+#       "There is no discount to apply."
+#     end
+#   end
+
+#   def void_last_transaction()
+#     @total -= @last_transaction_amount
+#   end
+
+# end
+
+# quantity.times do 
+#   @items << title
+# end
+require "pry"
 class CashRegister
-
-  attr_accessor :total, :discount, :last_transaction_amount, :items
-
+  
+  attr_accessor :discount, :total, :items, :last_transaction 
+   
   def initialize(discount = 0)
     @total = 0
     @discount = discount
     @items = []
   end
-
-  def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title   
-        i+=1
-      end
-    else
-      @items << title
+  
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+     quantity.times do 
+      @items << title 
     end
-    @total += price*quantity
-    @last_transaction_amount = @total
-    @total
+    @last_transaction = price * quantity 
   end
-
-  def apply_discount()
-    if @discount > 0
-      @discount = @discount/100.to_f
-      @total = @total - (@total * (@discount))
-      "After the discount, the total comes to $#{@total.to_i}."
-    else
+  
+  def apply_discount
+    if @discount == 0
       "There is no discount to apply."
-    end
+    else
+      employee_discount = @discount.to_f/100 * @total
+    @total -= employee_discount
+    "After the discount, the total comes to $#{@total.to_i}."
+   end
+  end
+  
+  def void_last_transaction
+   @total -= @last_transaction
   end
 
-  def void_last_transaction()
-    @total -= @last_transaction_amount
-  end
-
+  
+  
 end
-
-# quantity.times do 
-#   @items << title
-# end
