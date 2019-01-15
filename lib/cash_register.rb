fe7eda2b8dd015f7d344496ff969fc7b29d00944
Sara Walker <sarawall1187@gmail.com>
2019-01-15 20:23:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -104,9 +104,12 @@ class CashRegister
   end
   
   def apply_discount
-    employee_discount = @discount.to_f/100 * @total
+    if @discount == 0
+      employee_discount = @discount.to_f/100 * @total
     @total -= employee_discount
     "After the discount, the total comes to $#{@total.to_i}."
+  else
+    
   end
   
   
