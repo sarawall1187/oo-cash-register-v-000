79262abbddcee0e4f9e55256c1694f7cb7c2a656
Sara Walker <sarawall1187@gmail.com>
2019-01-15 20:23:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -105,13 +105,13 @@ class CashRegister
   
   def apply_discount
     if @discount == 0
-      
+      "There is no discount to apply."
     else
       employee_discount = @discount.to_f/100 * @total
     @total -= employee_discount
     "After the discount, the total comes to $#{@total.to_i}."
 
-    
+  end
   end
   
   
