c2c40c82bf45789a2e3a800f3428b995f785c206
Sara Walker <sarawall1187@gmail.com>
2019-01-15 20:40:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -105,6 +105,7 @@ class CashRegister
      quantity.times do 
       @items << title 
     end
+    @last_transaction = @total 
   end
   
   def apply_discount
@@ -118,6 +119,7 @@ class CashRegister
   end
   
   def void_last_transaction
+  
    @total -= @last_transaction
   end
 
