d91e359b598475c080c295b9d740a5a91db17862
Sara Walker <sarawall1187@gmail.com>
2019-01-15 20:30:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -102,7 +102,7 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     @total += price * quantity
-    @items << title * quantity
+    @items << title.times(quantity)
   end
   
   def apply_discount
